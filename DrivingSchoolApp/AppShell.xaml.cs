44a885480ddbb122407f084c3285eaafe40f917c
User <User@LAB3-3>
2024-10-07 15:12:24 +0300

    Add project files.

---
+++ DrivingSchoolApp/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace DrivingSchoolApp
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ DrivingSchoolApp/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace DrivingSchoolApp
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
