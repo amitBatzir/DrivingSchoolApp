29b86ee59912a41a1a228fd38f27e75098686476
User <User@LAB3-3>
2024-12-03 11:58:50 +0200

    Continue

--- DrivingSchoolApp/View/RegisterStudentView.xaml.cs
+++ DrivingSchoolApp/View/RegisterStudentView.xaml.cs
@@ -1,9 +1,12 @@
+using DrivingSchoolApp.ViewModels;
+
 namespace DrivingSchoolApp.View;
 
 public partial class RegisterView : ContentPage
 {
-	public RegisterView()
+	public RegisterView(RegisterStudentViewModel vm)
 	{
+		this.BindingContext = vm;
 		InitializeComponent();
 	}
 }
